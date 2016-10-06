017898ad2ef973b6b71195fa80a61e1d84d267dc
League <jiwenzou@gmail.com>
2016-10-06 16:52:49 -0700

    League Invaders
    just some mor invaders

--- src/Leauge_Invaders/GamePanel.java
+++ src/Leauge_Invaders/GamePanel.java
@@ -1,5 +1,7 @@
 package Leauge_Invaders;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,17 +13,50 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 Timer time = new Timer(1000/60, this);
-
+Font titleFont = new Font("Serif", Font.PLAIN, 48);
 final int MENU_STATE = 0;
 final int GAME_STATE = 1;
 final int END_STATE = 2;
+int currentState = 0;
 public void paintComponent(Graphics g){
-	.draw(g);
+	if(currentState == MENU_STATE){
+		drawMenuState(g);
+	}else if(currentState == GAME_STATE){
+		drawGameState(g);
+	}else if(currentState == END_STATE){
+		drawEndState(g);
+	}
+
 
 }
 void startGame(){
 	time.start();	
 	
+}
+void updateMenuState(){
+	
+}
+void updateGameState(){
+	
+}
+void updateEndState(){
+	
+}
+void drawMenuState(Graphics g){
+	System.out.println(titleFont.getFamily());
+	g.setColor(Color.BLUE);
+	g.fillRect(0, 0, Leauge_Invaders.gWidth, Leauge_Invaders.gHight);    
+	g.setFont(titleFont);
+	g.setColor(Color.GREEN);
+	g.drawString("League Invaders", 100, 300);
+}
+void drawGameState(Graphics g){
+	g.setColor(Color.BLACK);
+	g.fillRect(0, 0, Leauge_Invaders.gWidth, Leauge_Invaders.gHight);  
+}
+void drawEndState(Graphics g){
+	g.setColor(Color.RED);
+	g.fillRect(0, 0, Leauge_Invaders.gWidth, Leauge_Invaders.gHight);  
 }
 	
 	
@@ -31,17 +66,37 @@ void startGame(){
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 	repaint();
-	.update();
+	if(currentState == MENU_STATE){
+		
+		updateMenuState();
+	}else if(currentState == GAME_STATE){
+		updateGameState();
+	}else if(currentState == END_STATE){
+		updateEndState();
+	}
+	
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("Type");
-	}
+		
+		}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Press");
+		
+		if (e.getKeyCode()==KeyEvent.VK_ENTER){
+			System.out.println("enter");
+			if(currentState > 1){
+				System.out.println("change");
+				currentState = 0;
+			}
+			else{
+				currentState++;
+			}
+			
+
+		}
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
